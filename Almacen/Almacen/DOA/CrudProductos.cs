e416ba3c91c08ebd66c8f5060c6fc3ce6f83dcd8
Saul1500 <125324281+Saul1500@users.noreply.github.com>
2023-03-21 15:55:04 -0600

    Add files via upload

---
+++ Almacen/Almacen/DOA/CrudProductos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Almacen.Models;
+
+
+namespace Almacen.DAO
+{
+    internal class CrudProductos
+    {
+        public void AgregarUsuario(Producto ParametroProductos)
+        {
+
+            using (AlmacenContext db = new AlmacenContext())
+            {
+                Producto productos = new Producto();
+                productos.Nombre = ParametroProductos.Nombre;
+                productos.Descripcion = ParametroProductos.Descripcion;
+                productos.Precio = ParametroProductos.Precio;
+                productos.Stock = ParametroProductos.Stock;
+                db.Add(productos);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Almacen/Almacen/Program.cs
@@ -0,0 +1,79 @@
+﻿using Almacen.DAO;
+using Almacen.Models;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+
+    Console.WriteLine("======================================");
+    Console.WriteLine("BIENVENIDO AL MENU DE LA BASE DE DATOS");
+    Console.WriteLine("======================================");
+
+    Console.WriteLine("");
+
+    Console.WriteLine("Precio 1 para Insertar Productos");
+
+    Console.WriteLine("");
+
+    Console.WriteLine("Precio 0 para Cancelar la Operacion");
+
+    Console.WriteLine("======================================");
+    Console.WriteLine("");
+    
+    var menu = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("");
+
+switch (menu) {
+    case 1:
+        int bucle = 1;
+        while (bucle == 1)
+        {
+            Producto producto = new Producto();
+            CrudProductos crudProductos = new CrudProductos();
+
+            Console.WriteLine("Ingrese El Nombre del Producto: ");
+            producto.Nombre = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Ingrese la Descripcion del Producto: ");
+            producto.Descripcion = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Ingrese el Precio del Producto: ");
+            producto.Precio = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Ingrese la Cantidad de Stock: ");
+            producto.Stock = Convert.ToInt32(Console.ReadLine());
+
+            crudProductos.AgregarUsuario(producto);
+            Console.WriteLine("");
+
+            Console.WriteLine("Su Registro Ha Sido Guardado Exitosamente...");
+            Console.WriteLine("======================================");
+            Console.WriteLine("");
+
+            Console.WriteLine("======================================");
+            Console.WriteLine("BIENVENIDO AL MENU DE LA BASE DE DATOS");
+            Console.WriteLine("======================================");
+
+            Console.WriteLine("Precio 1 para Insertar Productos");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Precio 0 para Cancelar la Operacion");
+
+            bucle = Convert.ToInt32(Console.ReadLine());
+        }
+        break;
+
+    case 2:
+        Console.WriteLine("");
+        Console.WriteLine("Su Operacion ha Sido Cancelada");
+        break;
+}
+
+           
+         
+       
+
